b9acfe228b5f5cf01c09df1b54bc82b40d4d9afe
Jann Kleen <jann.kleen@gmail.com>
2024-03-21 08:56:13 +0100

    fix(nodes): Move periodic task creation to celery signal handler.
    This way we're not trying to create any database models before migrations ran.

--- backend/nodes/apps.py
+++ backend/nodes/apps.py
@@ -6,40 +6,5 @@ class ApiConfig(AppConfig):
     name = "nodes"
 
     def ready(self) -> None:
-        from django.conf import settings
-        from django_celery_beat.models import IntervalSchedule, PeriodicTask
-
-        # Create a schedule for the node event scanning task
-        schedule, created = IntervalSchedule.objects.get_or_create(
-            every=settings.NODE_CRDT_EVENTS_INTERVAL,
-            period=IntervalSchedule.SECONDS,
-        )
-
-        # Associate this schedule with your task
-        PeriodicTask.objects.update_or_create(
-            task=settings.NODE_CRDT_EVENTS_TASK,
-            defaults={
-                "interval": schedule,
-                "name": f"Scan document events every {settings.NODE_CRDT_EVENTS_INTERVAL} seconds",
-            },
-        )
-
-        # Create a schedule for the document versioning task
-        schedule, created = IntervalSchedule.objects.get_or_create(
-            every=settings.NODE_VERSIONING_TASK_INTERVAL,
-            period=IntervalSchedule.SECONDS,
-        )
-
-        # Associate this schedule with your task
-        PeriodicTask.objects.update_or_create(
-            task=settings.NODE_VERSIONING_TASK,
-            defaults={
-                "interval": schedule,
-                "name": f"Snapshot documents every {settings.NODE_VERSIONING_TASK_INTERVAL} "
-                f"seconds (snapshot interval is {settings.NODE_VERSIONING_INTERVAL}).",
-            },
-        )
-
-        # Import task here to avoid AppRegistryNotReady exception
         import nodes.signals
         import nodes.tasks  # noqa: F401
---
+++ backend/nodes/signals.py
@@ -0,0 +1,37 @@
+import typing
+
+from celery import signals
+from django.conf import settings
+from django_celery_beat.models import IntervalSchedule, PeriodicTask
+
+
+@signals.after_setup_task_logger.connect
+def create_periodic_tasks(sender: typing.Any, **kwargs: typing.Any) -> None:
+    # Create a schedule for the node event scanning task
+    schedule, created = IntervalSchedule.objects.get_or_create(
+        every=settings.NODE_CRDT_EVENTS_INTERVAL, period=IntervalSchedule.SECONDS
+    )
+
+    # Associate this schedule with your task
+    PeriodicTask.objects.update_or_create(
+        task=settings.NODE_CRDT_EVENTS_TASK,
+        defaults={
+            "interval": schedule,
+            "name": f"Scan document events every {settings.NODE_CRDT_EVENTS_INTERVAL} seconds",
+        },
+    )
+
+    # Create a schedule for the document versioning task
+    schedule, created = IntervalSchedule.objects.get_or_create(
+        every=settings.NODE_VERSIONING_TASK_INTERVAL, period=IntervalSchedule.SECONDS
+    )
+
+    # Associate this schedule with your task
+    PeriodicTask.objects.update_or_create(
+        task=settings.NODE_VERSIONING_TASK,
+        defaults={
+            "interval": schedule,
+            "name": f"Snapshot documents every {settings.NODE_VERSIONING_TASK_INTERVAL} "
+            f"seconds (snapshot interval is {settings.NODE_VERSIONING_INTERVAL}).",
+        },
+    )
