1413b3971ed9ffb6a2cc7f06e190ecf4ddd04f9d
Jann Kleen <jann.kleen@gmail.com>
2024-04-02 14:58:37 +0200

    fix(buddies): correctly calculate the cut-off level when querying a single node.
    Make sure that the cut-off (so we don't return more levels than necessary if the node structure doesn't go as deep) also works when there is only one node being queried.

--- backend/buddies/tests/factories.py
+++ backend/buddies/tests/factories.py
@@ -5,7 +5,7 @@ from factory.django import DjangoModelFactory
 class BuddyFactory(DjangoModelFactory):
     name = factory.Faker("sentence", nb_words=6)
     description = factory.Faker("text")
-    model = "gpt-3.5"
+    model = "gpt-3.5-turbo"
 
     class Meta:
         model = "buddies.Buddy"
