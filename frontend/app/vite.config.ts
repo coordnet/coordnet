a02670bdba465ccc5f97879201fc48ee8b021a53
Max Novakovic <max@maxnov.com>
2024-05-06 21:29:40 +0200

    build: Fix vite config for esnext

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -4,5 +4,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
+  esbuild: {
+    target: "esnext",
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
   plugins: [tsconfigPaths(), react()],
 });
