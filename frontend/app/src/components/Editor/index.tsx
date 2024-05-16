268b64dabd630f49506b88ee5f2da5b2e1de2ed6
Jann Kleen <jann.kleen@gmail.com>
2024-05-16 21:25:32 +0200

    fix(editor): Make editor not reload on every node refresh.

--- frontend/app/src/components/Editor/index.tsx
+++ frontend/app/src/components/Editor/index.tsx
@@ -52,7 +52,7 @@ const Editor = ({ id, className }: EditorProps) => {
       editorProps: { attributes: { class: "prose focus:outline-none" } },
       editable: Boolean(node?.allowed_actions.includes("write")),
     },
-    [id, node],
+    [id, node?.allowed_actions],
   );
 
   useEffect(() => {
