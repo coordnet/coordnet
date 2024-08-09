25bd7c2467c72541bf6bfdedc3a43edd5b9eb26f
Jann Kleen <jann.kleen@gmail.com>
2024-08-09 13:49:38 +0200

    fix(deployment): Close old database connections regularly.
    This is a fix specifically because of issues caused by django-channels.

--- backend/nodes/signals.py
+++ backend/nodes/signals.py
@@ -2,6 +2,7 @@ import typing
 
 from celery import signals
 from django.conf import settings
+from django.db import close_old_connections
 from django_celery_beat.models import IntervalSchedule, PeriodicTask
 
 
@@ -40,3 +41,9 @@ def create_periodic_tasks(sender: typing.Any, **kwargs: typing.Any) -> None:
             f"seconds (snapshot interval is {settings.NODE_VERSIONING_INTERVAL}).",
         },
     )
+
+
+# TODO: This should probably live in another place, not in a specific app.
+@signals.task_postrun.connect
+def close_old_database_connections(sender: typing.Any, **kwargs: typing.Any) -> None:
+    close_old_connections()
