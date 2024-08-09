0e08b142e877eac30596b7484046590755c2824d
Jann Kleen <jann.kleen@gmail.com>
2024-08-09 13:09:02 +0200

    feat(buddies): Move authentication of websocket connections into websocket messages.

--- backend/buddies/consumers.py
+++ backend/buddies/consumers.py
@@ -1,9 +1,11 @@
 import json
 
 import openai
+import rest_framework.exceptions
 from asgiref.sync import sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.conf import settings
+from knox.auth import TokenAuthentication
 
 import buddies.models
 import buddies.serializers
@@ -12,24 +14,37 @@ import buddies.serializers
 class QueryConsumer(AsyncWebsocketConsumer):
     """The same functionality as views/BuddyModelViewSet.query, but as a WebSocket consumer."""
 
-    async def connect(self) -> None:
-        if not self.scope["user"].is_authenticated:
-            await self.close(code=403)
-            return
-        try:
-            self.buddy = await buddies.models.Buddy.objects.aget(
-                public_id=self.scope["url_route"]["kwargs"]["public_id"]
-            )
-        except buddies.models.Buddy.DoesNotExist:
-            await self.close(code=404)
-        else:
-            await self.accept()
-
     async def receive(self, text_data: str | None = None, bytes_data: bytes | None = None) -> None:
         if text_data is None:
             return
+        payload = json.loads(text_data)
+        if (token := payload.get("token")) is None:
+            await self.close(code=1007)
+            return
 
-        serializer = buddies.serializers.BuddyQuerySerializer(data=json.loads(text_data))
+        # TokenAuthentication expects a byte string
+        token = token.encode()
+
+        token_auth = TokenAuthentication()
+        try:
+            user, _ = await sync_to_async(token_auth.authenticate_credentials)(token)
+        except rest_framework.exceptions.AuthenticationFailed:
+            await self.close(code=1008)
+            return
+
+        if user is None:
+            await self.close(code=1008)
+            return
+
+        try:
+            buddy = await buddies.models.Buddy.objects.aget(
+                public_id=self.scope["url_route"]["kwargs"]["public_id"]
+            )
+        except buddies.models.Buddy.DoesNotExist:
+            await self.close(code=1008)
+            return
+
+        serializer = buddies.serializers.BuddyQuerySerializer(data=payload)
         await sync_to_async(serializer.is_valid)(raise_exception=True)
         validated_data = serializer.validated_data
         nodes = validated_data.get("nodes")
@@ -41,7 +56,7 @@ class QueryConsumer(AsyncWebsocketConsumer):
         response = await openai.AsyncClient(
             api_key=settings.OPENAI_API_KEY
         ).chat.completions.create(
-            model=self.buddy.model,
+            model=buddy.model,
             messages=messages,
             stream=True,
             timeout=180,
