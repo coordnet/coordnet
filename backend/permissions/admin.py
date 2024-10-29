536fed3ec8773af6208a60cbcfcc16e807c9dea4
Max Novakovic <max@maxnov.com>
2024-10-29 20:09:11 +0100

    feat: Add permissions to the Space model in Django Admin

--- backend/permissions/admin.py
+++ backend/permissions/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 
 from permissions import models
+
+
+class ObjectMembershipInline(GenericTabularInline):
+    """
+    Inline admin interface for ObjectMembership.
+    Allows managing ObjectMembership instances within the parent model's admin page.
+    """
+
+    model = models.ObjectMembership
+    extra = 1
+
 
 admin.site.register(models.ObjectMembership)
 admin.site.register(models.ObjectMembershipRole)
