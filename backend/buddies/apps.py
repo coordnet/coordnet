78854238d70c9d2094ce29746deaf09c51dd6c98
Jann Kleen <jann.kleen@gmail.com>
2024-11-25 15:33:18 +0100

    refactor: Name AppConfig classes (more) correctly.

--- backend/buddies/apps.py
+++ backend/buddies/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+class BuddiesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "buddies"
 
--- backend/nodes/apps.py
+++ backend/nodes/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class ApiConfig(AppConfig):
+class NodesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "nodes"
 
