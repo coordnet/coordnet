0965a76657c00c147818db3256cbb8fa779d4463
Jann Kleen <jann.kleen@gmail.com>
2025-02-03 16:38:00 +0100

    fix(nodes): Fix N+1 query while fetching spaces.
    The default node wasn't pre-fetched.

--- backend/nodes/views.py
+++ backend/nodes/views.py
@@ -198,7 +198,7 @@ class SpaceModelViewSet(
 ):
     """API endpoint that allows projects to be viewed or edited."""
 
-    queryset = models.Space.available_objects.prefetch_related("nodes").all()
+    queryset = models.Space.available_objects.all()
     serializer_class = serializers.SpaceSerializer
     filter_backends = (filters.SpacePermissionFilterBackend, base_filters.BaseFilterBackend)
     permission_classes = (dry_permissions.DRYPermissions,)
@@ -208,7 +208,7 @@ class SpaceModelViewSet(
     ) -> "permissions.managers.SoftDeletableMembershipModelQuerySet[models.Space]":
         queryset = models.Space.available_objects.annotate(
             node_count=django_models.Count("nodes", filter=~django_models.Q(nodes__is_removed=True))
-        )
+        ).select_related("default_node")
         assert isinstance(queryset, permissions.managers.SoftDeletableMembershipModelQuerySet)
         return queryset.annotate_user_permissions(request=self.request)
 
