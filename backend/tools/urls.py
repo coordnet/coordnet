be08cfb474ad91704d202a7bb2ac3601c60a9f41
Jann Kleen <jann.kleen@gmail.com>
2025-02-17 16:15:29 +0100

    feat(nodes): Add tool execution endpoint for PaperQA.

---
+++ backend/tools/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+import tools.views
+from utils import routers
+
+app_name = "tools"
+
+router = routers.get_router()
+
+urlpatterns = router.urls
+urlpatterns += [
+    path("tools/paperqa/", tools.views.PaperQAView.as_view(), name="paperqa"),
+]
