33419e8d288f5a990694d15a6ab6082160b79585
Max Novakovic <max@maxnov.com>
2025-06-11 16:41:54 +0200

    fix(skills): Ensure all websockets are closed after reading

--- node/crdt/src/worker.ts
+++ node/crdt/src/worker.ts
@@ -6,25 +6,22 @@ import {
   CanvasNode,
   cleanSkillJson,
   createCanvas,
+  createConnectedYDocServer,
   createTasks,
   ExecutionContext,
+  logMemoryUsage,
   NodeType,
   processTasks,
   skillJsonToYdoc,
   skillYdocToJson,
 } from "@coordnet/core";
-import { HocuspocusProvider } from "@hocuspocus/provider";
 import * as celery from "@prd-thanhnguyenhoang/celery.node";
-import * as Y from "yjs";
 
 import { db } from "./db";
 import { setWorkerPrefetch } from "./utils";
 
 setInterval(() => {
-  const memoryUsage = process.memoryUsage();
-  console.log(
-    `Worker memory usage: RSS: ${Math.round(memoryUsage.rss / 1024 / 1024)}MB, Heap: ${Math.round(memoryUsage.heapUsed / 1024 / 1024)}/${Math.round(memoryUsage.heapTotal / 1024 / 1024)}MB`
-  );
+  logMemoryUsage("Worker");
 }, 30000);
 
 const worker = celery.createWorker(
@@ -39,20 +36,19 @@ worker.setOnFailed(async ({ body, error }) => {
   const skillRun = await db("nodes_methodnoderun").where("id", body[1].method_run_id).first();
   const skillRunId = skillRun?.public_id;
 
-  const doc = new Y.Doc();
-  const provider = new HocuspocusProvider({
-    url: process.env.HOCUSPOCUS_INTERNAL_URL,
-    name: `method-run-${skillRunId}`,
-    document: doc,
-    token: body[1].authentication,
-  });
+  const [doc, provider] = await createConnectedYDocServer(
+    `method-run-${skillRunId}`,
+    body[1].authentication
+  );
 
-  await new Promise<void>((resolve) => provider.on("synced", resolve));
-
-  const runMap = doc?.getMap("meta");
-  runMap.set("status", "error");
-  runMap.set("error", String(error));
-  console.error("Error executing method", error);
+  try {
+    const runMap = doc?.getMap("meta");
+    runMap.set("status", "error");
+    runMap.set("error", String(error));
+    console.error("Error executing method", error);
+  } finally {
+    provider.disconnect();
+  }
 });
 
 worker.register(
@@ -76,62 +72,68 @@ worker.register(
     const skillId = skill?.public_id;
     const skillRunId = skillRun?.public_id;
 
-    const doc = new Y.Doc();
-    new HocuspocusProvider({
-      url: process.env.HOCUSPOCUS_INTERNAL_URL,
-      name: `method-run-${skillRunId}`,
-      document: doc,
-      token: authentication,
-    });
+    const [doc, provider] = await createConnectedYDocServer(
+      `method-run-${skillRunId}`,
+      authentication
+    );
 
-    if (skillRun.method_data && Object.keys(skillRun.method_data).length > 0) {
-      await skillJsonToYdoc(skillRun.method_data, doc);
-    } else {
-      console.log("No method run data found, getting latest version");
-      const version = await db("nodes_methodnodeversion")
-        .where("method_id", methodId)
-        .orderBy("version", "desc")
-        .first();
-      await skillJsonToYdoc(version?.method_data, doc);
-    }
+    try {
+      if (skillRun.method_data && Object.keys(skillRun.method_data).length > 0) {
+        await skillJsonToYdoc(skillRun.method_data, doc);
+      } else {
+        console.log("No method run data found, getting latest version");
+        const version = await db("nodes_methodnodeversion")
+          .where("method_id", methodId)
+          .orderBy("version", "desc")
+          .first();
+        await skillJsonToYdoc(version?.method_data, doc);
+      }
 
-    const runMeta = doc?.getMap("meta");
-    if (!buddyId) {
-      buddyId = runMeta.get("buddy") as string;
-    }
-    const buddy = await db("buddies_buddy").where("public_id", buddyId).first();
-    if (!buddy) {
-      throw new Error("Buddy not found");
-    }
+      const runMeta = doc?.getMap("meta");
+      if (!buddyId) {
+        buddyId = runMeta.get("buddy") as string;
+      }
+      const buddy = await db("buddies_buddy").where("public_id", buddyId).first();
+      if (!buddy) {
+        throw new Error("Buddy not found");
+      }
 
-    if (method_argument) {
-      const inputId = await addInputNode(skillId, doc, method_argument);
-      console.log("Method argument found, added", inputId);
-    }
+      if (method_argument) {
+        const inputId = await addInputNode(skillId, doc, method_argument);
+        console.log("Method argument found, added", inputId);
+      }
 
-    // Get information from the skill document
-    const nodesMap: Y.Map<CanvasNode> = doc.getMap(`${skillId}-canvas-nodes`);
-    const edgesMap: Y.Map<CanvasEdge> = doc.getMap(`${skillId}-canvas-edges`);
-    const nodes = Array.from(nodesMap.values());
-    const canvas = createCanvas(nodes, Array.from(edgesMap.values()));
+      // Get information from the skill document
+      const nodesMap = doc.getMap<CanvasNode>(`${skillId}-canvas-nodes`);
+      const edgesMap = doc.getMap<CanvasEdge>(`${skillId}-canvas-edges`);
+      const nodes = Array.from(nodesMap.values());
+      const canvas = createCanvas(nodes, Array.from(edgesMap.values()));
 
-    const outputNode = nodes.find((node) => node.data.type === NodeType.Output);
-    if (!outputNode) {
-      console.error("Output node not found");
-      return;
-    }
+      const outputNode = nodes.find((node) => node.data.type === NodeType.Output);
+      if (!outputNode) {
+        console.error("Output node not found");
+        return;
+      }
 
-    if (runMeta.get("status") !== "cancelled") {
-      runMeta.set("status", "running");
-      const context: ExecutionContext = { authentication, taskList: [], responses: {}, outputNode };
-      createTasks(canvas, context);
-      await processTasks(context, buddy, skillId, doc, false);
-      console.log("Task running is completed");
-      runMeta.set("status", "success");
-      const skillJson = cleanSkillJson(skillYdocToJson(doc));
-      await db("nodes_methodnoderun").where("id", methodRunId).update({
-        method_data: skillJson,
-      });
+      if (runMeta.get("status") !== "cancelled") {
+        runMeta.set("status", "running");
+        const context: ExecutionContext = {
+          authentication,
+          taskList: [],
+          responses: {},
+          outputNode,
+        };
+        createTasks(canvas, context);
+        await processTasks(context, buddy, skillId, doc, false);
+        console.log("Task running is completed");
+        runMeta.set("status", "success");
+        const skillJson = cleanSkillJson(skillYdocToJson(doc));
+        await db("nodes_methodnoderun").where("id", methodRunId).update({
+          method_data: skillJson,
+        });
+      }
+    } finally {
+      provider.disconnect();
     }
   }
 );
